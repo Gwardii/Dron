2b12ade2c36c6fb6dcec22982c84c18a5fc46382
Gwardii <62251613+Gwardii@users.noreply.github.com>
2021-01-26 00:17:38 +0100

    Add files via upload

---
+++ ibus.h
@@ -0,0 +1,14 @@
+/*
+ * ibus.h
+ *
+ *  Created on: 25.01.2021
+ *      Author: filip
+ */
+
+#ifndef IBUS_H_
+#define IBUS_H_
+
+void USART2_IRQHandler();
+void Ibus_save();
+
+#endif /* IBUS_H_ */
