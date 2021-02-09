9b374e592b54e9a3ceaa10cdc3201637a0a469fe
SB-koperkowypiesek <76916080+SB-koperkowypiesek@users.noreply.github.com>
2021-02-09 21:12:45 +0100

    Add files via upload

--- acro.c
+++ acro.c
@@ -48,11 +48,11 @@ typedef struct {
 
 //---User defines maximum speed of spinning [deg/s]----
 
-Three Rates = { 700, 700, 200 };
+Three Rates = { 700, 700, 500 };
 
-PID R_PID = { 0.5, 0.0, 0 };
-PID P_PID = { 0.5, 0.0, 0 };
-PID Y_PID = { 0.5, 0.0, 0 };
+PID R_PID = { 0.5, 0.3, 0 };
+PID P_PID = { 0.5, 0.3, 0 };
+PID Y_PID = { 0.65, 0.3, 0 };
 
 Three err;
 Three sum_err = { 0, 0, 0 };
@@ -62,18 +62,20 @@ void acro() {
 	Three corr;
 
 
-	err.roll = (channels[0] - 1500) * 32768 - Gyro_Acc[0] * 1000 / Rates.roll;
-	err.pitch = (channels[1] - 1500) * 32768 - Gyro_Acc[1] * 1000 / Rates.pitch;
-	err.yaw = (channels[3] - 1500) * 32768 - Gyro_Acc[2] * 1000 / Rates.yaw;
+	err.roll = (channels[0] - 1500) * 32768/1000 - Gyro_Acc[0] * 1000 / Rates.roll;
+	err.pitch = (channels[1] - 1500) * 32768/1000 - Gyro_Acc[1] * 1000 / Rates.pitch;
+	err.yaw = (channels[3] - 1500) * 32768/1000 - Gyro_Acc[2] * 1000 / Rates.yaw;
 
 	//	estimate Integral by sum (I term):
 	sum_err.roll += err.roll;
 	sum_err.pitch += err.pitch;
 	sum_err.yaw += err.yaw;
 
-	corr.pitch = (P_PID.P * err.pitch + P_PID.I*sum_err.pitch)/ 32768;
-	corr.roll = (R_PID.P * err.roll + R_PID.I*sum_err.roll)/ 32768;
-	corr.yaw = (Y_PID.P * err.yaw + Y_PID.I*sum_err.yaw)/ 32768;
+	anti_windup();
+
+	corr.pitch = (P_PID.P * err.pitch + P_PID.I*sum_err.pitch)*1000/ 32768;
+	corr.roll = (R_PID.P * err.roll + R_PID.I*sum_err.roll)*1000/ 32768;
+	corr.yaw = (Y_PID.P * err.yaw + Y_PID.I*sum_err.yaw)*1000/ 32768;
 
 //	right front:
 	PWM_M1 = Throttle - corr.pitch + corr.yaw - corr.roll;
@@ -87,16 +89,6 @@ void acro() {
 	//	left front:
 	PWM_M4 = Throttle - corr.pitch - corr.yaw + corr.roll;
 
-	//anti_windup();
-}
-
-void anti_windup() {
-	if (PWM_M1 > 2000 || PWM_M2 > 2000 || PWM_M3 > 2000 || PWM_M4 > 2000 || PWM_M1 < 1050 || PWM_M2 < 1050 || PWM_M3 < 1050 || PWM_M4 < 1050) {
-		sum_err.roll -= err.roll;
-		sum_err.pitch -= err.pitch;
-		sum_err.yaw -= err.yaw;
-	}
-
 	if (PWM_M1 < 1050) {
 		PWM_M1 = 1050;
 	}
@@ -109,6 +101,26 @@ void anti_windup() {
 	if (PWM_M4 < 1050) {
 		PWM_M4 = 1050;
 	}
+}
+
+void anti_windup() {
+	int16_t max_I_correction = 300;
+	if (sum_err.roll * R_PID.I > max_I_correction) {
+		sum_err.roll = max_I_correction / R_PID.I;
+	} else if (sum_err.roll * R_PID.I < -max_I_correction) {
+		sum_err.roll = -max_I_correction / R_PID.I;
+	}
+	if (sum_err.pitch * P_PID.I > max_I_correction) {
+		sum_err.pitch = max_I_correction / P_PID.I;
+	} else if (sum_err.pitch * P_PID.I < -max_I_correction) {
+		sum_err.pitch = -max_I_correction / P_PID.I;
+	}
+	if (sum_err.yaw * Y_PID.I > max_I_correction) {
+		sum_err.yaw = max_I_correction / Y_PID.I;
+	} else if (sum_err.yaw * Y_PID.I < -max_I_correction) {
+		sum_err.yaw = -max_I_correction / Y_PID.I;
+	}
+
 }
 
 static uint16_t micros() {
